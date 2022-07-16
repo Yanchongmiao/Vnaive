e000cafbe6a016a937532a7ca59a410736219deb
yc6 <17737266888@163.com>
2022-07-16 15:08:08 +0800

    style(deps): 恢复状态 => axios api

--- src/router/backRoutes/index.ts
+++ src/router/backRoutes/index.ts
@@ -1,25 +1,24 @@
-// import { oldbackpermissions } from '@/api'
-// import { routeStore } from '@/pinia/modules/routeStore'
-// import { createRoutes } from '../utils/createRoutes'
-// import { removeRoute } from '../utils/remove'
+import { oldbackpermissions } from '@/api'
+import { routeStore } from '@/pinia/modules/routeStore'
+import { createRoutes } from '../utils/createRoutes'
+import { removeRoute } from '../utils/remove'
 const modules = import.meta.globEager('./**/*.ts')
 export const bacoRoutersMap = new Map()
 export const getBackRoutes = async function () {
-  // try {
-  //   let {
-  //     data: { route, allAuth },
-  //   } = await oldbackpermissions()
-  //   oldbackpermissions()
-  //   removeRoute()
-  //   const routeStoreValue = routeStore()
-  //   routeStoreValue.$patch({
-  //     originalData: route,
-  //     auth: allAuth,
-  //   })
-  //   createRoutes()
-  // } catch (error) {
-  //   console.error(`Back模式获取权限失败:`, error)
-  // }
+  try {
+    let {
+      data: { route, allAuth },
+    } = await oldbackpermissions()
+    removeRoute()
+    const routeStoreValue = routeStore()
+    routeStoreValue.$patch({
+      originalData: route,
+      auth: allAuth,
+    })
+    createRoutes()
+  } catch (error) {
+    console.error(`Back模式获取权限失败:`, error)
+  }
 }
 export const backStaticRoutes = async function () {
   Object.keys(modules).forEach((item: string) => {
