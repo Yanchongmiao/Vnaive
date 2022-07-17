25b3b656d3055ec7251fad9bf0d80b7fe2539164
yc6 <17737266888@163.com>
2022-07-17 19:35:48 +0800

    ci(deps): 删除多余文件

--- commitlint.config.js
+++ commitlint.config.js
@@ -7,18 +7,18 @@ module.exports = {
       2,
       'always',
       [
-        'perf', //性能优化
         'feat', // 新功能
         'fix', //  修复
         'docs', // 文档变更
+        'resou', //资源变更
         'style', // 代码格式（不影响代码运行的变动）
         'refactor', // 重构（既不是增加feature）,也不是修复bug
+        'perf', //性能优化
         'test', // 增加测试
+        'build', // 打包
         'ci', //修改 CI 配置、脚本
         'chore', // 构建过程或辅助工具的变动
         'revert', // 回退
-        'build', // 打包
-        'resou', //资源变更
         'deps', //项目依赖
       ],
     ],
